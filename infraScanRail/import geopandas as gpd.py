1e4f47db2c7eb76c7b9c8cf7170c3366df43549f
Silvano Fuchs <sifuchs@ethz.ch>
2025-04-08 15:27:18 +0200

    Deleted unused files in the project

--- infraScanRail/import geopandas as gpd.py
+++
@@ -1,8 +0,0 @@
-import geopandas as gpd
-from tabulate import tabulate
-
-# Read the network dataset
-network = gpd.read_file(r"data/temp/network_railway-services.gpkg")
-
-# Print the DataFrame as a table
-print(tabulate(network.head(), headers='keys', tablefmt='psql'))  # 'psql' gives a nice table format
