4aca20b4919e9dba9bb086e357ba2c71a34bf23b
earnor <arnor17@gmail.com>
2025-04-28 11:55:48 +0200

    add OD_prep.py to improve handling of tazs outside the defined extent

--- infraScanRail/paths.py
+++ infraScanRail/paths.py
@@ -1,2 +1,4 @@
 MAIN = r"D:\ETH_Msc\MA\infraScan\infraScanRail"
 RAIL_SERVICES_AK2035_PATH= r'data\temp\railway_services_ak2035.gpkg'
+#MAIN = r"C:/Users/spadmin/PycharmProjects/infraScan/infraScanRail"
+
